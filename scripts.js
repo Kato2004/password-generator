8cc2f16351b31fa1a5f87f9be654bf37e2187143
Kato2004 <kauaalvesnascimento22@gmail.com>
2023-02-28 21:34:06 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+const span = document.getElementById("a");
+const form = document.getElementById("form");
+const btnCopy = document.getElementById("btn-copy");
+const inputLength = document.getElementById("input-pw-length");
+const inputCharsUppercase = document.getElementById("uppercase-chars");
+const inputCharsLowercase = document.getElementById("lowercase-chars");
+const inputCharsNumber = document.getElementById("numbers-chars");
+const inputCharsSpecial = document.getElementById("special-chars");
+const showPassword = document.getElementById("show-password");
+
+let pwLength;
+let password = "";
+let chars = "";
+let charsNumbers = "0123456789";
+let charsLowercase = "abcdefghijklmnopqrstuvwxyz";
+let charsUppercase = "ABCDEFGHIJLMNOPQRSTUVWXYZ";
+let charsSpecial = "!@#$%^&*()+?><:{}[]";
+
+inputLength.value = 8;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  password = "";
+  if (inputCharsUppercase.checked) chars += charsUppercase;
+  if (inputCharsLowercase.checked) chars += charsLowercase;
+  if (inputCharsNumber.checked) chars += charsNumbers;
+  if (inputCharsSpecial.checked) chars += charsSpecial;
+
+  if (inputLength.value >= 8 && inputLength.value <= 64) {
+    pwLength = inputLength.value;
+
+    for (let i = 0; i < pwLength; i++) {
+      const randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber + 1);
+    }
+
+    showPassword.innerHTML = password;
+    chars = "";
+    password = "";
+  }
+});
+
+btnCopy.addEventListener("click", () => {
+  navigator.clipboard.writeText(showPassword.innerHTML);
+});
